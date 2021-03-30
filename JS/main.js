6d9e79a80a2db235eac15afb5a94c1906eeb6d33
daudi13 <davouma13@gmail.com>
2021-03-30 11:17:52 +0300

    commit previous changes

--- JS/main.js
+++ JS/main.js
@@ -5,3 +5,8 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
     output.innerHTML = this.value
 }
+
+sliderRange.addEventListener("input", () =>
+{
+    sliderRange.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${sliderRange.value / 32 * 100}%, hsl(224, 65%, 95%) ${sliderRange.value / 32 * 100}%, hsl(224, 65%, 95%) 100%)`;
+});
