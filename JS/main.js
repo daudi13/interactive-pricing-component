3cda47afbbe23bf983e4ea7ced8c07f756b89a29
daudi13 <davouma13@gmail.com>
2021-04-02 04:47:33 +0300

    commit complete js

--- JS/main.js
+++ JS/main.js
@@ -10,7 +10,7 @@ sliderRange.addEventListener("input", () =>
 {
         rate.textContent = "$" +
         sliderRange.value + ".00";
-    rateMobile.textContent = "$" +
-        sliderRange.value + ".00";
+        rateMobile.textContent = "" +
+        sliderRange.value + "";
     sliderRange.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${sliderRange.value / 32 * 100}%, hsl(224, 65%, 95%) ${sliderRange.value / 32 * 100}%, hsl(224, 65%, 95%) 100%)`;
 });
