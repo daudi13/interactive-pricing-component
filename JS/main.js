edbeb0d89dcb84b87307a63a1543c1e1c6ff43de
daudi13 <davouma13@gmail.com>
2021-04-02 04:37:29 +0300

    commit functional javascript

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,16 @@
-const slider = document.getElementById("TheRange");
-const output = document.getElementById("rate");
-const slider = document.getElementById()
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-    output.innerHTML = this.value
+function el(e) {
+    return document.querySelector(e);
 }
+
+const sliderRange = el("#TheRange");
+const rate = el("#rate");
+const rateMobile = el("#rateMobil")
 
 sliderRange.addEventListener("input", () =>
 {
+        rate.textContent = "$" +
+        sliderRange.value + ".00";
+    rateMobile.textContent = "$" +
+        sliderRange.value + ".00";
     sliderRange.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${sliderRange.value / 32 * 100}%, hsl(224, 65%, 95%) ${sliderRange.value / 32 * 100}%, hsl(224, 65%, 95%) 100%)`;
 });
